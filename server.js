73a114b4754da4403e288fcdab9ec23729280e10
Raghavender Mylagary <w981756@usm.edu>
2017-04-18 13:34:24 -0500

    Video live rendering

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ var http = require('http'),
     fs = require('fs');
 
 
-fs.readFile('./blender.html', function (err, html) {
+fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err;
     }
     http.createServer(function(request, response) {
         response.writeHeader(200, {"Content-Type": "text/html"});
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+        response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
         response.write(html);
         response.end();
     }).listen(8000);
